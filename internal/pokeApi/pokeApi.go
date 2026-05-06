cdaefbba583d0e7e58dec469823ba9c7fb2e8831
Jessie Pearson <223473112+slonepearson@users.noreply.github.com>
2026-05-06 13:07:02 +0200

    added a GET request function to PokeApi

---
+++ internal/pokeApi/pokeApi.go
@@ -0,0 +1,42 @@
+package pokeApi
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type areaLocations struct {
+	Count    int     `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+func GetLocationAreas(url string) (areaLocations, error) {
+	if url == "" {
+		url = "https://pokeapi.co/api/v2/location-area/"
+	}
+
+	res, err := http.Get(url)
+	if err != nil {
+		return areaLocations{}, fmt.Errorf("network error: %v", err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return areaLocations{}, fmt.Errorf("non ok GET request: %v", res.Status)
+	}
+
+	defer res.Body.Close()
+	decoder := json.NewDecoder(res.Body)
+
+	var locationAreas areaLocations
+	if err = decoder.Decode(&locationAreas); err != nil {
+		return areaLocations{}, fmt.Errorf("issue parsing json: %v", err)
+	}
+
+	return locationAreas, nil
+}
