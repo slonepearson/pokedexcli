16d34de390b2a38d4e3aa7957b58be95126c0266
Jessie Pearson <223473112+slonepearson@users.noreply.github.com>
2026-05-06 10:00:22 +0200

    setup REPL

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		ok := scanner.Scan()
+		if !ok {
+			err := scanner.Err()
+			if err != nil {
+				fmt.Printf("Error while scanning Stdin: %v", err)
+				break
+			}
+		}
+		userInput := scanner.Text()
+		if strings.TrimSpace(userInput) == "" {
+			continue
+		}
+		cleanedWords := cleanInput(userInput)
+		command := cleanedWords[0]
+
+		fmt.Printf("Your command was: %s\n", command)
+	}
 }
